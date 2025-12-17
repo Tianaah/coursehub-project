03f5561da6d3f214b96b5c33421bb51bd13af418
Tianaah <christiana15ako@gmail.com>
2025-12-17 22:37:01 +0100

    new updates

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react(), svgr()],
+
+  resolve: {
+    alias: {
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
 });
